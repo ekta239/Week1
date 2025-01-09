fd21d0bd5ef8647feb390ccc3e7f55d0ddf64680
Ekta verma <ektav8231@gmail.com>
2025-01-09 21:28:01 +0530

    update

--- Level1/Problem05.java
+++ Level1/Problem05.java
@@ -1,6 +1,11 @@
 public class Problem05 {
     public static void main(String[] args) {
         // Define the number of pens and students
-        int pens = 14, stu = 3;
+        int pens = 14, stu = 3; // Number of pens and students
         
-        // Calculate the number of pens each student
+        // Calculate the number of pens each student can get
+        int pensPerStu = pens / stu; // Integer division to get the whole number of pens per student
+        int remainder = pens % stu; // Calculate the remaining pens that cannot be evenly distributed
+        System.out.println("The Pen Per Student is "+pensPerStu+" and the remaining pen not distributed is "+remainder);
+    }
+}
--- Level1/Problem10.java
+++ Level1/Problem10.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
-/**
- * This class solves problem 10 by converting height in centimeters to feet and inches.
- */
 public class Problem10 {
+    // Main method 
     public static void main(String[] args) {
-        // Create a new Scanner object to read user input
+        
+        // Create a Scanner object to read user input
         Scanner sc = new Scanner(System.in);
 
         // Prompt the user to enter their height in centimeters
         System.out.print("Enter height in cm: ");
 
-        // Read the user's height in centimeters
+        // Read the user's height from the console
         double height = sc.nextDouble();
 
         // Convert the height from centimeters to inches
-        double
+        double inches = height/2.54;
+
+        // Convert the height from inches to feet
+        double feet = inches/12;
+        
+        // Print the user's height in centimeters, feet, and inches
+        System.out.println("Your Height in cm is "+height+" while in feet is "+feet+" and inches is "+inches);
+    }
+}
